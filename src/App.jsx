eae1c438c6cd3dbf5fdae69f6103d1ea7c4e6198
anjaliBaditya <anjalibaditya8@gmail.com>
2024-07-13 14:20:02 +0000

    autopyaui 4

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Joystick from './components/Joystick';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Joystick />
+    </div>
+  )
+}
